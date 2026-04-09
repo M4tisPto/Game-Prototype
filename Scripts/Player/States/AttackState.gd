9f54ceea0edb12b76001b1c4b5d15efbebae79d8
M4tisPto <matis.pto.104250@gmail.com>
2026-04-09 11:21:58 -0400

    attack state start

--- Scripts/Player/States/AttackState.gd
+++ Scripts/Player/States/AttackState.gd
@@ -2,3 +2,9 @@ extends State
 
 func get_state_name():
 	return "Attack"
+@onready var timer: Timer = $Timer
+
+
+func _input(event: InputEvent) -> void:
+	if(event.is_action_pressed("attack_button")):
+		timer.start()
